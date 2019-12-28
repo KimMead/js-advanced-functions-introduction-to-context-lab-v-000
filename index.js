22304f72f46e46713fb4e4b93b098344ec1f4cbe
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-28 21:24:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const createEmployeeRecord = (arr) => {
   return {
     firstName: arr[0],
-
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
 }
